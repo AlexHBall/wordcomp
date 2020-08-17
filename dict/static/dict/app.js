bfdbec7cc92e2479a382772464f15ca0b00263e3
AlexHBall <timmygaines22@gmail.com>
2020-08-17 16:37:05 +0200

    add word container, remove on click

--- dict/static/dict/app.js
+++ dict/static/dict/app.js
@@ -2,15 +2,23 @@ $(document).ready(function () {
   var words = [];
 
   function add_word() {
+    function on_word_click(e) {
+      var target = $(e.target);
+      const text = target.text();
+      const index = words.indexOf(text);
+      if (index > -1) {
+        words.splice(index, 1);
+      }
+      $(".words-hidden").val(words.join(" "));
+      target.remove();
+    }
     var word = $(".words-input").val().replace(/ /g, "");
     words.push(word);
     $(".words-input").val("");
     $(".words-hidden").val(words.join(" "));
-    var ps = $(".words-container");
-    var d = $("<div>", { id: "word-" + word, text: word, class: "added-word" });
-    ps.append(d);
-    //    var w = $( "[id*='word-']" )
-    //   console.log(w)
+    var d = $("<p>", { id: "word-" + word, text: word, class: "added-word" });
+    d.click(on_word_click);
+    $(".words-container").append(d);
   }
   $(".words-input").keypress(function (e) {
     if (e.keyCode == 32) {

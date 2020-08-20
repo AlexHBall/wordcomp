6352109d059d21f0d3aae6f93b2d22c4fa0c3b5e
AlexHBall <timmygaines22@gmail.com>
2020-08-20 10:09:58 +0200

    add basic delete word link

--- dict/static/dict/app.js
+++ dict/static/dict/app.js
@@ -4,20 +4,31 @@ $(document).ready(function () {
   function add_word() {
     function on_word_click(e) {
       var target = $(e.target);
+      var parent = target.parent();
       const text = target.text();
       const index = words.indexOf(text);
       if (index > -1) {
         words.splice(index, 1);
       }
       $(".words-hidden").val(words.join(" "));
-      target.remove();
+      parent.remove();
     }
     var word = $(".words-input").val().replace(/ /g, "");
     words.push(word);
     $(".words-input").val("");
     $(".words-hidden").val(words.join(" "));
-    var d = $("<div>", { id: "word-" + word, text: word, class: "added-word" });
-    d.click(on_word_click);
+    var d = $("<div>", {
+      id: "word-" + word,
+      text: word + " ",
+      class: "added-word",
+    });
+    var a = $("<a>", {
+      href: "javascript:void(0)",
+      text: "x",
+      class: "delete-word",
+    });
+    a.click(on_word_click);
+    d.append(a);
     $(".words-container").append(d);
   }
   $(".words-input").keypress(function (e) {

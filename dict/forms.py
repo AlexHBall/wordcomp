04316d919336fe53863d7c85fbf2954bafe18288
AlexHBall <timmygaines22@gmail.com>
2020-08-20 18:33:59 +0200

    reset for css

--- dict/forms.py
+++ dict/forms.py
@@ -8,7 +8,7 @@ class EnterWordsForm(forms.Form):
     # words = forms.CharField(label='', max_length=100,
     #                         widget=forms.CharField(attrs={'class': 'words-input'}))
 
-    words = forms.CharField(label='Enter words to compare', max_length=100,
+    words = forms.CharField(label='Words', max_length=100,
                             widget=forms.TextInput(attrs={'class': 'words-input'}), required=False)
     to_search = forms.CharField(widget=forms.HiddenInput(
         attrs={'class': 'words-hidden'}), required=False)
